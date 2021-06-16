205ca94a0efadea6c00d711c606567b60656c21f
tthenne <troy@hashinteractive.com>
2021-06-16 16:43:23 -0500

    initial commit

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Plugin Name:       Kirkwood 
+ * Plugin URI:        https://gitlab.com/tthenne/kirkwood-plugin 
+ * Description:       Custom Plugin for Kirkwood functionalty to extend WordPress.
+ * Version:           1.0
+ * Author:            Hash Interactive 
+ * Author URI:        https://hashinteractive.com/
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:       kirkwood 
+ */
